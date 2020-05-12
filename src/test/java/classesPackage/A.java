0a9b22c12c260dff297ac7a6a7c856c0d15873b1
Firooz Housainee <mirhousainee@gmail.com>
2020-05-12 11:01:39 -0700

    Some printing done

---
+++ src/test/java/classesPackage/A.java
@@ -0,0 +1,56 @@
+package classesPackage;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class A {
+  @Test
+  public void f() {
+	  
+	  System.out.println("Is it working?");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  
+	  System.out.println("Before method");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("After method");
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("Before class");
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  System.out.println("After class");
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+  }
+
+}
