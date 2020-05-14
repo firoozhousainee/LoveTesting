35ddd36905d9d60985117b6177f766413629ad3f
Firooz Housainee <mirhousainee@gmail.com>
2020-05-13 17:05:05 -0700

    Groups, Runnining tests, or classes in Parallel via .xml file

--- src/test/java/classesPackage/Listeners.java
+++ src/test/java/classesPackage/Listeners.java
@@ -17,38 +17,38 @@ public class Listeners implements ITestListener {
 	
 	public void onTestSuccess(ITestResult result) {
 		
-		System.out.println("Test Successfully Finished" +result.getName());
+		System.out.println("Test Successfully Finished" + result.getName());
 		
 	}
  
 	public void onTestFailure(ITestResult result) {
 		
-		System.out.println("Test Failed" +result.getName());
+		System.out.println("Test Failed" + result.getName());
 		
 	}
  
 	public void onTestSkipped(ITestResult result) {
 		
-		System.out.println("Test Skipped" +result.getName());
+		System.out.println("Test Skipped" + result.getName());
 		
 	}
  
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 		
-		System.out.println("Test Failed but within success percentage" +result.getName());
+		System.out.println("Test Failed but within success percentage" + result.getName());
 		
 	}
  
 	public void onStart(ITestContext context) {
 		
 		
-		System.out.println("This is onStart method" +context.getOutputDirectory());
+		System.out.println("This is onStart method" + context.getOutputDirectory());
 		
 	}
  
 	public void onFinish(ITestContext context) {
 			
-		System.out.println("This is onFinish method" +context.getPassedTests());
-		System.out.println("This is onFinish method" +context.getFailedTests());
+		System.out.println("This is onFinish method" + context.getPassedTests());
+		System.out.println("This is onFinish method" + context.getFailedTests());
 	}
 }
