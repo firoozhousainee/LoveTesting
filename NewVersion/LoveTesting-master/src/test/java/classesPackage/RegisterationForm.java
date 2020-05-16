9f761f7d61e8df90cbabbd1a4a9d57c199cf7223
Firooz Housainee <mirhousainee@gmail.com>
2020-05-15 17:25:30 -0700

    Is Jenkins integerating???

---
+++ NewVersion/LoveTesting-master/src/test/java/classesPackage/RegisterationForm.java
@@ -0,0 +1,10 @@
+package classesPackage;
+
+import org.testng.annotations.Test;
+
+public class RegisterationForm {
+  @Test
+  public void f() {
+	  System.out.println("Need to configure the form");
+  }
+}
