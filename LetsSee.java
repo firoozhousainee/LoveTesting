76f795bf2ede272af59a568b41655bce8d6f706e
Firooz Housainee <mirhousainee@gmail.com>
2020-05-15 17:21:21 -0700

    LetSee class added

---
+++ LetsSee.java
@@ -0,0 +1,30 @@
+package testsuite;
+
+import org.testng.annotations.Test;
+
+
+public class A extends Parent {
+	
+	
+	@Test
+	public void three() {
+		
+		System.out.println("Test 3");
+	}
+	
+	
+	@Test
+	public void one() {
+		System.out.println("Test 1");
+		
+	}
+	
+	@Test
+	public void four() {
+		
+		System.out.println("Test 4");
+		System.out.println("I JenKins updating??????????? ")
+		
+	}
+
+}
